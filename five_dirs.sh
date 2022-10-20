836379e2941c49994d5d7fa0f0ebaa3f944e8d7a
Yeming Wei <ywei83@wisc.edu>
2022-10-20 14:05:51 -0500

    final

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 mkdir five
 cd five 
-for i in $(seq 1 5);do
+for i in $(seq 1 5); do
 	mkdir dir$i
 	cd dir$i
-	for j in $(seq 1 4);do
+	for j in $(seq 1 4); do
 		for ((k=1 ; k<=$j ; k++)); do
 			echo $j >> file$j
-		done
-		
+		done		
 	done
 	cd ..
 done
