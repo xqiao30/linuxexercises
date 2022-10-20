912eca8823ae823314736d13fd89a2dca6c2145b
Yeming Wei <ywei83@wisc.edu>
2022-10-20 13:57:31 -0500

    change

--- five_dirs.sh
+++ five_dirs.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+mkdir five
+cd five 
+for i in $(seq 1 5);do
+	mkdir dir$i
+	cd dir$i
+	for j in $(seq 1 4);do
+		for ((k=1 ; k<=$j ; k++)); do
+			echo $j >> file$j
+		done
+		
+	done
+	cd ..
+done
