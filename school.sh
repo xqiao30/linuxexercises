54a6be762c2df3a1e4dfa7d66948e2c09fc20bef
Yeming Wei <ywei83@wisc.edu>
2022-10-20 13:55:52 -0500

    change

--- school.sh
+++ school.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cat Property_Tax_Roll.csv | \
+grep "MADISON SCHOOLS" | \
+cut -d , -f 7 | \
+    { sum=0; count=0; while read n; do count=$(($count + 1)); sum=$(($sum + $n)); done; mean=$(echo "scale=6; $sum/$count" | bc); echo $mean;} 
+
