c5d49bf4285d4bcbfc2fa72d898618ada692c864
Xilin Qiao <xqiao24@wisc.edu>
2022-10-21 12:50:07 -0500

    finish

--- mean.sh
+++ mean.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+if [[ $# == [12] ]]
+then
+    echo "usage: $0 <column> [file.csv]" 1>&2
+    exit 0
+fi
+column=$1
+if [[ $# -ne 1 ]]
+then
+    file=/dev/stdin/
+else
+    file=$2
+fi
+cut -d, -f $column $file | tail -n +2 | {
+    count=0
+    sum=0
+    while read n
+    do
+	count=$(($count + 1))
+	sum=$(echo "$sum + $n" | bc)
+    done
+    mean=$(echo "scale=3; $sum/$count" | bc)
+    echo "$mean"
+}
